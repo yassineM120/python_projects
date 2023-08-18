02c693aed3b83515b2a781a8d04b3f3566b0a499
Yassine Mardoume <124624529+yassineM120@users.noreply.github.com>
2023-08-18 17:00:44 +0100

    Update and rename game.py to word_guessing_game.py

--- Beginner/word_guessing_game.py
+++ Beginner/word_guessing_game.py
@@ -5,7 +5,7 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+#print(f'Pssst, the solution is {chosen_word}.')
 
 
 display = []
